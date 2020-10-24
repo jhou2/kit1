d767e5bd3493309aaf476cf118b6729551b3d6fd
Joseph Hou <hou.joseph@gmail.com>
2020-10-24 08:32:34 -0700

    fix message

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -35,10 +35,45 @@ axios({
         email: email,
         message: message,
     }
-  }).then((response) => { console.log(response); })
+  }).then((response) => { 
+      console.log(response); 
+                          // Success message
+                          $("#success").html("<div class='alert alert-success'>");
+                          $("#success > .alert-success")
+                              .html(
+                                  "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                              )
+                              .append("</button>");
+                          $("#success > .alert-success").append(
+                              "<strong>Your message has been sent. </strong>"
+                          );
+                          $("#success > .alert-success").append("</div>");
+                          //clear all fields
+                          $("#contactForm").trigger("reset");
+    })
+  .catch((error) => {
+      console.log(error);
+                          // Fail message
+                          $("#success").html("<div class='alert alert-danger'>");
+                          $("#success > .alert-danger")
+                              .html(
+                                  "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                              )
+                              .append("</button>");
+                          $("#success > .alert-danger").append(
+                              $("<strong>").text(
+                                  "Sorry " +
+                                      firstName +
+                                      ", it seems that my mail server is not responding. Please try again later!"
+                              )
+                          );
+                          $("#success > .alert-danger").append("</div>");
+                          //clear all fields
+                          $("#contactForm").trigger("reset");
+  });
 
   // Original Ajax
-            $.ajax({
+            /* $.ajax({
                 url: "https://app.99inbound.com/api/e/aNsUaVtv",
                 type: "POST",
                 headers: {
@@ -91,7 +126,7 @@ axios({
                         $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
                     }, 1000);
                 },
-            });
+            }); */
         },
         filter: function () {
             return $(this).is(":visible");
